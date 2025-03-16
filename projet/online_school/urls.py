c8268a131d028da60e4fc774be87b305694331cf
EmmanuelTAHI <emmanueltahi14@gmail.com>
2025-03-16 18:20:02 +0000

    Modification de urls.py

--- projet/online_school/urls.py
+++ projet/online_school/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name="index"),
+    path('', views.index, name="index"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("courses/", views.courses, name="courses"),
